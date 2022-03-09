d1f5678198d979d58b57448063c1b3471e1bbe68
Saltssaumure <2239197L@student.gla.ac.uk>
2022-03-09 02:47:42 +0000

    Add teacher url mapping and view

--- CourseMate/coursemateapp/urls.py
+++ CourseMate/coursemateapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'coursemateapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    
+    path('teacher/', views.teacher, name='teacher'),
 ]
--- CourseMate/coursemateapp/views.py
+++ CourseMate/coursemateapp/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def teacher(request):
+    return render(request, 'teacher.html')

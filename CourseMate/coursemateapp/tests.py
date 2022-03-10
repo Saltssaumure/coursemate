be0b531ea5d1ff6283d4db5dff4b715441a18e95
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-03-10 10:32:47 +0000

    initial setup for tests

--- CourseMate/coursemateapp/tests.py
+++ CourseMate/coursemateapp/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass 
+    
+    def setUp(self):
+        print("setUp: RUn once for every test method to setup clean data.")
+        pass 
+    
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+    
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(False)
+    
+    def test_one_plus_one_equals_two(self):
+        print("Method: test)one_plus_one_equals_two.")
+        self.assertEquals(1 + 1, 2)
+    
+    

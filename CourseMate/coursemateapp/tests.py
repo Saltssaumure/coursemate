e6ac822098740b5014194d22a80ed9422df36443
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-03-10 10:51:49 +0000

    tests for student and teacher models

--- CourseMate/coursemateapp/tests.py
+++ CourseMate/coursemateapp/tests.py
@@ -1,27 +1,49 @@
 from django.test import TestCase
 
-# Create your tests here.
+from coursemateapp.models import Student, Teacher
 
-class TestClass(TestCase):
+# Create your tests here.
+    
+class StudentTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass 
+        # set up non-modified objects used by test methods 
+        Student.objects.create(student_ID=12345)
+
+    def test_student_ID(self):
+        student = Student.objects.get(id=1)
+        student_ID = student._meta.get_field('student_ID').verbose_name
+        self.assertEquals(student_ID, 'student ID')
     
-    def setUp(self):
-        print("setUp: RUn once for every test method to setup clean data.")
-        pass 
+    def test_student_ID_max_length(self):
+        student = Student.objects.get(id=1)
+        max_length = student._meta.get_field('student_ID').max_length
+        self.assertEquals(max_length, 8) 
+
+    def test_ID_is_ID(self):
+        student = Student.objects.get(id=1)
+        expected_ID = "12345"
+        self.assertEqual(str(student), expected_ID)
     
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+class TeacherTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # set up non-modified objects used by test methods 
+        Teacher.objects.create(teacher_ID=54321)
     
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
+    def test_teacher_ID(self):
+        teaher = Teacher.objects.get(id=1)
+        teacher_ID = Teacher._meta.get_field('teacher_ID').verbose_name
+        self.assertEquals(teacher_ID, 'teacher ID')
     
-    def test_one_plus_one_equals_two(self):
-        print("Method: test)one_plus_one_equals_two.")
-        self.assertEquals(1 + 1, 2)
-    
-    
+    def test_teacher_ID_max_length(self):
+        teaher = Teacher.objects.get(id=1)
+        max_length = Teacher._meta.get_field('teacher_ID').max_length
+        self.assertEquals(max_length, 8)
+
+    def test_ID_is_ID(self):
+        teacher = Teacher.objects.get(id=1)
+        expected_ID = "54321"
+        self.assertEqual(str(teacher), expected_ID)       
+
+

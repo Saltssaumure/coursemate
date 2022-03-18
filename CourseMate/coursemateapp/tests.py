039ff6fada1ff46808f79cc7ff24a4f830bbadc9
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-03-18 12:29:02 +0000

    tests for Assignment model

--- CourseMate/coursemateapp/tests.py
+++ CourseMate/coursemateapp/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from coursemateapp.models import Student, Teacher
+from coursemateapp.models import Student, Teacher, Course, Assignment
 
 # Create your tests here.
     
@@ -24,6 +24,27 @@ class StudentTest(TestCase):
         student = Student.objects.get(id=1)
         expected_ID = "12345"
         self.assertEqual(str(student), expected_ID)
+
+class StudentTestFailCases(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # set up non-modified objects used by test methods 
+        Student.objects.create(student_ID=12345)
+
+    def test_student_isnt_ID(self):
+        student = Student.objects.get(id=1)
+        student_ID = student._meta.get_field('student_ID').verbose_name
+        self.assertNotEquals(student_ID, 'student_ID')
+    
+    def test_student_ID_isnt_max_length(self):
+        student = Student.objects.get(id=1)
+        max_length = student._meta.get_field('student_ID').max_length
+        self.assertNotEquals(max_length, 10) 
+
+    def test_ID_isnt_ID(self):
+        student = Student.objects.get(id=1)
+        expected_ID = "33333"
+        self.assertNotEqual(str(student), expected_ID)
     
 class TeacherTest(TestCase):
     @classmethod
@@ -32,12 +53,12 @@ class TeacherTest(TestCase):
         Teacher.objects.create(teacher_ID=54321)
     
     def test_teacher_ID(self):
-        teaher = Teacher.objects.get(id=1)
+        teacher = Teacher.objects.get(id=1)
         teacher_ID = Teacher._meta.get_field('teacher_ID').verbose_name
         self.assertEquals(teacher_ID, 'teacher ID')
     
     def test_teacher_ID_max_length(self):
-        teaher = Teacher.objects.get(id=1)
+        teacher = Teacher.objects.get(id=1)
         max_length = Teacher._meta.get_field('teacher_ID').max_length
         self.assertEquals(max_length, 8)
 
@@ -46,16 +67,186 @@ class TeacherTest(TestCase):
         expected_ID = "54321"
         self.assertEqual(str(teacher), expected_ID)  
 
-#test URLS 
-class StudentTestView(TestCase):
+class TeacherTestFailCases(TestCase):
     @classmethod
     def setUpTestData(cls):
         # set up non-modified objects used by test methods 
         Teacher.objects.create(teacher_ID=54321)
     
-    def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/student/')
-        self.assertEqual(response.status_code, 200)
+    def test_teacher_isnt_ID(self):
+        teacher = Teacher.objects.get(id=1)
+        teacher_ID = Teacher._meta.get_field('teacher_ID').verbose_name
+        self.assertNotEquals(teacher_ID, 'teacher_ID')
+    
+    def test_teacher_ID_isnt_max_length(self):
+        teacher = Teacher.objects.get(id=1)
+        max_length = Teacher._meta.get_field('teacher_ID').max_length
+        self.assertNotEquals(max_length, 10)
+
+    def test_ID_isnt_ID(self):
+        teacher = Teacher.objects.get(id=1)
+        expected_ID = "33333"
+        self.assertNotEqual(str(teacher), expected_ID) 
+
+class CourseTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # set up non-modified objects used by test methods 
+        teacher_data = Teacher.objects.create(teacher_ID=54321)
+        student_data = Student.objects.create(student_ID=12345)
+        course = Course.objects.create(teacher=teacher_data, course_ID=1010, 
+            name="WAD2", description="Web App Development")
+        course.student.add(student_data)
+    
+    # id tests 
+    def test_course_ID(self):
+        course = Course.objects.get(id=1)
+        course_ID = Course._meta.get_field('course_ID').verbose_name
+        self.assertEquals(course_ID, 'course ID')
+    
+    def test_course_ID_max_length(self):
+        course = Course.objects.get(id=1)
+        max_length = Course._meta.get_field('course_ID').max_length
+        self.assertEquals(max_length, 15)
+
+    def test_ID_is_ID(self):
+        course_ID = Course.objects.get(id=1)
+        expected_ID = "1010"
+        self.assertEqual(str(course_ID), expected_ID) 
+
+    # name tests 
+    def test_course_name(self):
+        course = Course.objects.get(id=1)
+        course_name = Course._meta.get_field('name').verbose_name
+        self.assertEqual(course_name, 'name' )
+    
+    def test_course_name_max_length(self):
+        course = Course.objects.get(id=1)
+        max_length = Course._meta.get_field('name').max_length
+        self.assertEquals(max_length, 30)
+    
+    def test_name_is_name(self):
+        course = Course.objects.get(id=1).name
+        expected_name = "WAD2"
+        self.assertEqual(str(course), expected_name) 
+    
+    # description tests 
+    def test_course_description_name(self):
+        course= Course.objects.get(id=1)
+        course_desc = Course._meta.get_field('description').verbose_name
+        self.assertEquals(course_desc, 'description')
+    
+    def test_course_description_max_length(self):
+        course = Course.objects.get(id=1)
+        max_length = Course._meta.get_field('description').max_length
+        self.assertEquals(max_length, 1000)
+    
+    def test_course_description_is_description(self):
+        course = Course.objects.get(id=1)
+        expected_desc = "Web App Development"
+        self.assertEquals(expected_desc, "Web App Development")
+
+class CourseTestFailCases(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # set up non-modified objects used by test methods 
+        teacher_data = Teacher.objects.create(teacher_ID=54321)
+        student_data = Student.objects.create(student_ID=12345)
+        course = Course.objects.create(teacher=teacher_data, course_ID=1010, 
+            name="WAD2", description="Web App Development")
+        course.student.add(student_data)
+    
+    # id tests 
+    def test_course_isnt_ID(self):
+        course = Course.objects.get(id=1)
+        course_ID = Course._meta.get_field('course_ID').verbose_name
+        self.assertNotEquals(course_ID, 'course_ID')
+    
+    def test_course_ID_wrong_max_length(self):
+        course = Course.objects.get(id=1)
+        max_length = Course._meta.get_field('course_ID').max_length
+        self.assertNotEquals(max_length, 20)
+
+    def test_ID_isnt_ID(self):
+        course_ID = Course.objects.get(id=1)
+        expected_ID = "3333"
+        self.assertNotEqual(str(course_ID), expected_ID) 
+
+    # name tests 
+    def test_course_isnt_name(self):
+        course = Course.objects.get(id=1)
+        course_name = Course._meta.get_field('name').verbose_name
+        self.assertNotEqual(course_name, 'Name' )
+    
+    def test_course_name_wrong_max_length(self):
+        course = Course.objects.get(id=1)
+        max_length = Course._meta.get_field('name').max_length
+        self.assertNotEquals(max_length, 15)
+    
+    def test_name_isnt_name(self):
+        course = Course.objects.get(id=1).name
+        expected_name = "ADS2"
+        self.assertNotEqual(str(course), expected_name) 
+    
+    # description tests 
+    def test_course_description_wrong_name(self):
+        course= Course.objects.get(id=1)
+        course_desc = Course._meta.get_field('description').verbose_name
+        self.assertNotEquals(course_desc, 'Description')
+    
+    def test_course_description_max_length(self):
+        course = Course.objects.get(id=1)
+        max_length = Course._meta.get_field('description').max_length
+        self.assertNotEquals(max_length, 50)
+    
+    def test_course_description_isnt_description(self):
+        course = Course.objects.get(id=1)
+        expected_desc = "Algorithms and Data Structures 2"
+        self.assertNotEquals(expected_desc, "Web App Development")
+    
+class AssignmentTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # set up non-modified objects used by test methods 
+        teacher_data = Teacher.objects.create(teacher_ID=54321)
+        student_data = Student.objects.create(student_ID=12345)
+        course_data = Course.objects.create(teacher=teacher_data, course_ID=1010, 
+            name="WAD2", description="Web App Development")
+        course_data.student.add(student_data)
+        assignment = Assignment.objects.create(course=course_data, name="Tango with Django")
+
+    def test_assignment_name_max_length(self):
+        assignment = Assignment.objects.get(id=1)
+        max_length = assignment._meta.get_field('name').max_length
+        self.assertEquals(max_length, 20) 
+
+    def test_name_is_name(self):
+        assignment = Assignment.objects.get(id=1)
+        expected_name = "Tango with Django"
+        self.assertEqual(str(assignment), expected_name)
+
+class AssignmentTestFailCases(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # set up non-modified objects used by test methods 
+        teacher_data = Teacher.objects.create(teacher_ID=54321)
+        student_data = Student.objects.create(student_ID=12345)
+        course_data = Course.objects.create(teacher=teacher_data, course_ID=1010, 
+            name="WAD2", description="Web App Development")
+        course_data.student.add(student_data)
+        assignment = Assignment.objects.create(course=course_data, name="Tango with Django")
+
+    def test_assignment_name_wrong_max_length(self):
+        assignment = Assignment.objects.get(id=1)
+        max_length = assignment._meta.get_field('name').max_length
+        self.assertNotEquals(max_length, 30) 
+
+    def test_name_isnt_name(self):
+        assignment = Assignment.objects.get(id=1)
+        expected_name = "Group Project"
+        self.assertNotEqual(str(assignment), expected_name)
+    
+
 
 
 

f59878f7bd73c49b80f4c791004aa9905a8bee69
Zhikun Chen <2612803c@student.gla.ac.uk>
2022-03-18 09:45:54 +0000

    Database update

--- CourseMate/coursemateapp/models.py
+++ CourseMate/coursemateapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Student(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     student_ID = models.CharField(max_length=8)
@@ -10,10 +11,52 @@ class Student(models.Model):
     def __str__(self):
         return self.student_ID
 
+
 class Teacher(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     teacher_ID = models.CharField(max_length=8)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
+    picture = models.ImageField(upload_to='', blank=True)
 
     def __str__(self):
         return self.teacher_ID
+
+
+class Course(models.Model):
+    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE)
+    course_ID = models.CharField(max_length=15)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000)
+    student = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.course_ID
+
+
+class Assignment(models.Model):
+    course = models.ForeignKey(Course, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Has(models.Model):
+    student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
+    assignment = models.ForeignKey(Assignment, null=True, on_delete=models.CASCADE)
+    PDF = models.FileField(upload_to='', blank=True)
+    Grade = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.student.student_ID + ' ' + self.assignment.name
+
+
+class Review(models.Model):
+    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
+    score = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.student.student_ID + ' to ' + self.teacher.teacher_ID
+
+
+
